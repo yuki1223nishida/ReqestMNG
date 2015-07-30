699df6455cfd3f08adeda3cdcf2072f8920ded74
yuki1223nishida <axel.yuki@gmail.com>
2015-07-30 23:31:26 +0900

    index update

--- index.php
+++ index.php
@@ -1,13 +1,51 @@
 <!DOCTYPE html>
 <html >
-<head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta charset="utf-8">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>リクエスト</title>
-</head>
-<body>
-  
-</body>
+  <head>
+    <meta charset="utf-8">
+    <title>リクエスト</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge chrome=1" />
+    <meta http-equiv="Cache-Control" content="no-cache,no-store">
+    <meta name="robots" content="noindex,nofollow">
+    <link href="common/css/style.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script type="text/javascript" src="common/js/jquery.lightbox-0.5.min.js"></script>
+    <script type="text/javascript" src="common/js/jquery-1.9.1"></script>
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="headerWrapper">
+        <div id="header">
+          <!-- ロゴ画像 ここから -->
+          
+          <!-- ロゴ画像 ここまで -->
+          <!-- キャッチコピー ここから -->
+          <div class="description"></div>
+          <!-- キャッチコピー ここまで -->
+          <div class="headerAddress">
+            <!-- （ヘッダー） ここから -->
+            <table >
+              <tr>
+                <td style="text-align:right">
+                  <p >ようこそ　○○様</p>
+                </td>
+                <td>&nbsp;&nbsp;</td>
+                <td style="vertical-align:middle; text-align:center;">
+                  <form method="post" action="logout.asp" target="_top">
+                    <input type="submit" value="ログアウト">
+                  </form>
+                </td>
+              </tr>
+            </table>
+            <!-- （ヘッダー） ここまで -->
+          </div>
+        </div>
+      </div>
+      <div id="menu">
+      </div>
+      <div id="footerWrapper">
+      </div>
+    </div>
+  </body>
 </html>
