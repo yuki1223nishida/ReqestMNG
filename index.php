19a2d8a043fd9f81375343fc53fcc284e52b394a
yuki1223nishida <axel.yuki@gmail.com>
2015-08-01 00:01:35 +0900

    index update

--- index.php
+++ index.php
@@ -1,50 +1,59 @@
+<?php
+  include('./common/src/DBConnect.php');
+?>
 <!DOCTYPE html>
-<html >
+<html lang="ja">
   <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>リクエスト</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
     <meta http-equiv="X-UA-Compatible" content="ie=edge chrome=1" />
-    <meta http-equiv="Cache-Control" content="no-cache,no-store">
-    <meta name="robots" content="noindex,nofollow">
-    <link href="common/css/style.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript" src="common/js/jquery.lightbox-0.5.min.js"></script>
-    <script type="text/javascript" src="common/js/jquery-1.9.1"></script>
+    <script type="text/javascript" src="common/js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="common/js/tsCheck.js"></script>
+    <link rel="stylesheet" type="text/css" href="common/css/tsStyle.css" media="all" />
+    <link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/ui-lightness/jquery-ui.css" rel="stylesheet" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/i18n/jquery.ui.datepicker-ja.min.js"></script>
+    <script type="text/javascript">
+      <!--
+        jQuery( function() {
+           if($("#datepickerST").val() == ""){
+            $("#datepickerST").datepicker();
+            $("#datepickerST").datepicker('setDate', 'today');
+          }else{
+            $("#datepickerST").datepicker();
+            $("#datepickerST").datepicker('setDate', $("#datepickerST").val());
+          }
+          $("#timepickerST").timepicker({ 'timeFormat': 'H:i'});
+          $("#timepickerEN").timepicker({ 'timeFormat': 'H:i'});
+          $("#jquery-ui-datepickerEN").datepicker();
+          $("#jquery-ui-datepickerEN").datepicker("option", {"dateFormat": "yy/mm/dd"});
+          $("#jquery-ui-datepickerEN").datepicker('setDate', 'today');
+        } );
+      // -->
+    </script>
   </head>
   <body>
-    <div id="wrapper">
-      <div id="headerWrapper">
-        <div id="header">
-          <!-- ロゴ画像 ここから -->
-          
-          <!-- ロゴ画像 ここまで -->
-          <!-- キャッチコピー ここから -->
-          <div class="description"></div>
-          <!-- キャッチコピー ここまで -->
-          <div class="headerAddress">
-            <!-- （ヘッダー） ここから -->
-            <table >
-              <tr>
-                <td style="text-align:right">
-                  <p >ようこそ　○○様</p>
-                </td>
-                <td>&nbsp;&nbsp;</td>
-                <td style="vertical-align:middle; text-align:center;">
-                  <form method="post" action="logout.asp" target="_top">
-                    <input type="submit" value="ログアウト">
-                  </form>
-                </td>
-              </tr>
-            </table>
-            <!-- （ヘッダー） ここまで -->
-          </div>
-        </div>
+    <div id="header" class="mod_form">
+      <div class="description"></div>
+        <table >
+        <table>
+          <tr>
+            <th>
+              <div class="item_title">依頼日時<br /></div>
+              <div class="item_sub_title">
+                日付&nbsp;<span class="ex_essential" >*</span><br />
+                時間&nbsp;&nbsp;&nbsp;
+              </div>
+            </th>
+            <td>
+              <br />
+              <input type="text" id="datepickerST" name="startdate_s"  /><br />
+              <input type="time" id="timepickerST" name="starttime_s" value="" placeholder="hh:mm" />
+            </td>
+          </tr>
+        </table>
       </div>
-      <div id="menu">
-      </div>
-      <div id="footerWrapper">
+      <div id="">
       </div>
     </div>
   </body>
