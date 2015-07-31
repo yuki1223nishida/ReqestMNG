48a7eefee096055b7f2765af62741398fd6f9e96
yuki1223nishida <axel.yuki@gmail.com>
2015-08-01 00:10:17 +0900

    index update2

---
+++ common/src/DBConnect.php
@@ -0,0 +1,9 @@
+<?php
+  // DBコネクト情報
+  define('IHOST',       'localhost');
+  define('IPORT',       '1180');
+  define('IDBNAME',     'REQESTMNG');
+  define('IUSER',       'yuki');
+  define('IPASS',       'rounat825D');
+  define('ICONNECTION', 'host=' . IHOST . ' port=' . IPORT . ' dbname=' . IDBNAME . ' user=' . IUSER . ' password=' . IPASS);
+?>
